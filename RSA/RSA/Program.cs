6cb479eee3059a8d2a344c1b167d54d49b706fe4
AyAztuB <ayaztub@gmail.com>
2022-06-23 16:53:54 +0200

    Complete GenerateKeys option

--- RSA/RSA/Program.cs
+++ RSA/RSA/Program.cs
@@ -14,20 +14,8 @@ namespace RSA
                 DisplayHelp();
             else
                 DisplayHelp(args[0][2..]);*/
-            DirectoryInfo d = new DirectoryInfo("./");
-            Console.WriteLine(d.FullName);
-            Console.WriteLine(Directory.Exists("/NOPE/RATIO/UWU"));
-            System.Console.WriteLine(args.Length);
-            for (int i = 0; i < args.Length; i++){
-                System.Console.WriteLine(i + " " + args[i].Length + " " + args[i]);
-            }
-            System.Console.WriteLine("END");
-            string path = "/toto/nope/../COUCOU";
-            string[] split = path.Split('/');
-            System.Console.WriteLine(split.Length);
-            for (int i = 0; i < split.Length; i++){
-                System.Console.WriteLine(i + " " + split[i].Length + " " + split[i]);
-            }
+            if (args.Length != 0 && args[0] == "--GenerateKeys")
+                GenerateKeysReadOption(args);
         }
 
 
@@ -40,7 +28,7 @@ namespace RSA
             Console.WriteLine("This program can create RSA keys, decrypt and encrypt messages using the RSA crypto-system and can break some very small RSA keys (only in source code) !\n");
             if (option == "")
             {
-                System.Console.WriteLine("run <OPTION> [-h] [-c pathToCommonKeyPart] [-k keyPath] [-m message] [-f File] [-x] [-o filename] [-j useJson] [-r nbRound] [-l RSAlength]");
+                System.Console.WriteLine("run <OPTION> [--h] [--c pathToCommonKeyPart] [--k keyPath] [--m message] [--f File] [--x] [--o filename] [--j useJson] [--r nbRound] [--l RSAlength]");
                 System.Console.WriteLine();
                 System.Console.WriteLine("Available OPTIONs :");
                 System.Console.WriteLine();
@@ -53,40 +41,40 @@ namespace RSA
                 System.Console.WriteLine("Currently in development\nNot yet available !\nThanks for your understanding !\n");
             else if (option == "GenerateKeys")
             {
-                System.Console.WriteLine("run --GenerateKeys [-h] [-c path_to_common_key_part] [-k keyPath] [-j jsonFile] [-r nbRound] [-l RSAlength]\n");
+                System.Console.WriteLine("run --GenerateKeys [--h] [--c path_to_common_key_part] [--k keyPath] [--j jsonFile] [--r nbRound] [--l RSAlength]\n");
                 System.Console.WriteLine("Available arguments :\n");
-                System.Console.WriteLine("-h \t=> display this help ^^\n");
-                System.Console.WriteLine("-j \t=> transform RSA object in json file\n   \t if the file exists, it will be overridden, otherwise, it will be created !\n");
-                System.Console.WriteLine("-c \t=> the file where the encoded common part of the keys will be written\n   \t if the file exists, it will be overridden, otherwise, it will be created !\n");
-                System.Console.WriteLine("-k \t=> the file where the private encoded key will be written. The public key will be written in the same location with .pub extension");
-                System.Console.WriteLine("   \t if the file exists, it will be overridden, otherwise, it will be created !\n");
-                System.Console.WriteLine("-r \t=> number of rounds for the prime number generation [default : 30]\n");
-                System.Console.WriteLine("-l \t=> length of the prime numbers generated [default : 256 bytes ~ 616 digits]\n");
+                System.Console.WriteLine("--h \t=> display this help ^^\n");
+                System.Console.WriteLine("--j \t=> transform RSA object in json file\n   \t if the file exists, it will be overridden, otherwise, it will be created !\n");
+                System.Console.WriteLine("--c \t=> the file where the encoded common part of the keys will be written\n   \t if the file exists, it will be overridden, otherwise, it will be created !\n");
+                System.Console.WriteLine("--k \t=> the file where the private encoded key will be written. The public key will be written in the same location with .pub extension");
+                System.Console.WriteLine("    \t if the file exists, it will be overridden, otherwise, it will be created !\n");
+                System.Console.WriteLine("--r \t=> number of rounds for the prime number generation [default : 30]\n");
+                System.Console.WriteLine("--l \t=> length of the prime numbers generated [default : 256 bytes ~ 616 digits]\n");
                 System.Console.WriteLine("OTHERS ARGUMENTS ARE UNAVAILABLE !!!!\n");
-                System.Console.WriteLine("-c and -k cannot be used without the other one, you can use [-c <name> -k <name>] and [-j <name>] to save the key multiple time.");
-                System.Console.WriteLine("If you don't use [-c <name> -k <name>] neither [-j <name>], all the keys will be displayed in the encoded format in the console.");
+                System.Console.WriteLine("--c and --k cannot be used without the other one, you can use [--c <name> --k <name>] and [--j <name>] to save the key multiple time.");
+                System.Console.WriteLine("If you don't use [--c <name> --k <name>] neither [--j <name>], all the keys will be displayed in the encoded format in the console.");
                 System.Console.WriteLine("If an argument is incorrect or if the path given is invalid, this help page will be displayed.\n");
                 System.Console.WriteLine("The argument order doesn't have any importance.\n");
             }
             else
             {
-                System.Console.WriteLine($"run --{option} [-h] [-c path_to_common_key_part] [-k keyPath] [-j jsonFile] [-m message] [-f file] [-x] [-o outputName]\n");
+                System.Console.WriteLine($"run --{option} [--h] [--c path_to_common_key_part] [--k keyPath] [--j jsonFile] [--m message] [--f file] [--x] [--o outputName]\n");
                 System.Console.WriteLine("Available arguments :\n");
-                System.Console.WriteLine("-h \t=> display this help ^^\n");
-                System.Console.WriteLine("-c \t=> the file where the encoded common part of the key has been written\n");
-                System.Console.WriteLine($"-k \t=> the file where the {(option == "Encrypt" ? "public" : "private")} key has been written\n");
-                System.Console.WriteLine("-j \t=> the json file where the RSA object has been saved\n");
-                System.Console.WriteLine("-m \t=> message to " + option.ToLower() + '\n');
-                System.Console.WriteLine("-f \t=> the file (or directory with -x necessarily) to " + option.ToLower() + '\n');
-                System.Console.WriteLine("-x \t=> used to encrypt all files in a given directory. Cannot be applied without -f");
+                System.Console.WriteLine("--h \t=> display this help ^^\n");
+                System.Console.WriteLine("--c \t=> the file where the encoded common part of the key has been written\n");
+                System.Console.WriteLine($"--k \t=> the file where the {(option == "Encrypt" ? "public" : "private")} key has been written\n");
+                System.Console.WriteLine("--j \t=> the json file where the RSA object has been saved\n");
+                System.Console.WriteLine("--m \t=> message to " + option.ToLower() + '\n');
+                System.Console.WriteLine("--f \t=> the file (or directory with --x necessarily) to " + option.ToLower() + '\n');
+                System.Console.WriteLine("--x \t=> used to encrypt all files in a given directory. Cannot be applied without --f");
                 if (option == "Encrypt")
                     System.Console.WriteLine("   \t    if it's applied to a large file, it forces the encryption, splitting the file content and encrypting each part. The output will be a directory (with the given name) where each part will be encrypted in a file <name>(number) [without extension]");
-                System.Console.WriteLine("\n-o \t=> precise an output file or directory. If it's a directory, each " + option.ToLower()+"ed file will be added or overridden in the output directory with the same name as the original file.\n");
+                System.Console.WriteLine("\n--o \t=> precise an output file or directory. If it's a directory, each " + option.ToLower()+"ed file will be added or overridden in the output directory with the same name as the original file.\n");
                 System.Console.WriteLine("OTHERS ARGUMENTS ARE UNAVAILABLE !!!!\n");
-                System.Console.WriteLine("-c and -k cannot be used without the other one, you cannot use [-c <name> -k <name>] and [-j <name>] at the same time.");
-                System.Console.WriteLine("One of the two options ([-j] or [-c -k]) is necessary !");
-                System.Console.WriteLine("If [-o] isn't precise, if [-m] is used, then the output will be displayed in the terminal, else the output will override the given file !");
-                System.Console.WriteLine("-f and -m cannot be used at the same time.");
+                System.Console.WriteLine("--c and --k cannot be used without the other one, you cannot use [--c <name> --k <name>] and [--j <name>] at the same time.");
+                System.Console.WriteLine("One of the two options ([--j] or [--c --k]) is necessary !");
+                System.Console.WriteLine("If [--o] isn't precise, if [--m] is used, then the output will be displayed in the terminal, else the output will override the given file !");
+                System.Console.WriteLine("--f and --m cannot be used at the same time.");
                 System.Console.WriteLine("If an argument is incorrect or if the path/file given is invalid, this help page will be displayed.\n");
                 System.Console.WriteLine("The argument order doesn't have any importance.\n");
             }
@@ -112,7 +100,7 @@ namespace RSA
             {
                 switch (args[i])
                 {
-                    case "-j":
+                    case "--j":
                         if (json != "" || i+1 >= args.Length || args[i+1].Length == 0){
                             DisplayHelp(help);
                             return;
@@ -120,7 +108,7 @@ namespace RSA
                         i+= 1;
                         json = args[i];
                         break;
-                    case "-c":
+                    case "--c":
                         if (common != "" || i+1 >= args.Length || args[i+1].Length == 0){
                             DisplayHelp(help);
                             return;
@@ -128,7 +116,7 @@ namespace RSA
                         i+=1;
                         common = args[i];
                         break;
-                    case "-k":
+                    case "--k":
                         if (key != "" || i+1 >= args.Length || args[i+1].Length == 0){
                             DisplayHelp(help);
                             return;
@@ -136,7 +124,7 @@ namespace RSA
                         i+=1;
                         key = args[i];
                         break;
-                    case "-r":
+                    case "--r":
                         if (i+1 >= args.Length || args[i+1].Length == 0){
                             DisplayHelp(help);
                             return;
@@ -160,7 +148,7 @@ namespace RSA
                             return;
                         }
                         break;
-                    case "-l":
+                    case "--l":
                         if (i+1 >= args.Length || args[i+1].Length == 0){
                             DisplayHelp(help);
                             return;
@@ -208,10 +196,109 @@ namespace RSA
                 DisplayHelp(help);
                 return;
             }
+            if (common != "" && !IsPathCorrect(common)){
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("FAIL: THE GIVEN COMMON FILE CANNOT BE ACCESSED DU TO THE INCORRECT PATH");
+                System.Console.WriteLine("complete path: " + GetCompletePath(common));
+                Console.ForegroundColor = ConsoleColor.White;
+                DisplayHelp(help);
+                return;
+            }
+            if (key != "" && !IsPathCorrect(key)){
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("FAIL: THE GIVEN KEY FILE CANNOT BE ACCESSED DU TO THE INCORRECT PATH");
+                System.Console.WriteLine("complete path: " + GetCompletePath(key));
+                Console.ForegroundColor = ConsoleColor.White;
+                DisplayHelp(help);
+                return;
+            }
+            if (common != "" && common[^1] == '/')
+                common += "common";
+            if (key != ""){
+                if (key[^1] == '/')
+                    key += "rsa";
+                else if(key.Split('/')[^1].Contains('.')){
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("FAIL: THE GIVEN KEY FILE HAVE AN EXTENSION !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    DisplayHelp(help);
+                    return;
+                }
+            }
+            if (json != ""){
+                if (json[^1] == '/')
+                    json += "RSA.json";
+                else {
+                    string last = json.Split('/')[^1];
+                    if (last.Length < 6 || last[^5..] != ".json"){
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("FAIL: THE GIVEN JSON FILE HAVE A BAD EXTENSION !");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        DisplayHelp(help);
+                        return;
+                    }
+                }
+            }
             RSA rsa = new RSA(length, rounds);
+            if (json != ""){
+                if (WriteRSAFile(rsa, json)){
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("Successfully create .json rsa file");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("FAIL: IMPOSSIBLE TO WRITE RSA IN JSON FILE !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            if (key != "" && common != ""){
+                if (WriteFile(common, rsa.commonKeyParameter, true)){
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("Successfully write common key parameter");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("FAIL: IMPOSSIBLE TO WRITE RSA COMMON KEY PARAMETER !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (WriteFile(key, rsa.privKey, true)){
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("Successfully write private key parameter");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("FAIL: IMPOSSIBLE TO WRITE RSA PRIVATE KEY PARAMETER !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (WriteFile(key+".pub", rsa.pubKey, true)){
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.WriteLine("Successfully write public key parameter");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("FAIL: IMPOSSIBLE TO WRITE RSA PUBLIC KEY PARAMETER !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            else if (json == ""){
+                Console.WriteLine("COMMON RSA KEY:");
+                System.Console.WriteLine(rsa.commonKeyParameter + '\n');
+                System.Console.WriteLine("PUBLIC KEY:\n"+rsa.pubKey+'\n');
+                System.Console.WriteLine("PRIVATE KEY:\n"+rsa.privKey+'\n');
+            }
         }
 
         #endregion
+
+        #region Encrypt-Decrypt
+        #endregion
+
+        #endregion
+
 
         private static bool IsPathCorrect(string path){
             if (path[^1] == '/')
@@ -249,12 +336,6 @@ namespace RSA
             }
             return res;
         }
-
-        #region Encrypt-Decrypt
-        #endregion
-        // TODO
-
-        #endregion
 
 
         #region RSA_from_json
